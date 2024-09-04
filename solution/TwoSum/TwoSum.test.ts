77e7e622b044b088d58115ada925928660a2b723
Ice Maliwan <nantawat@magicboxsolution.com>
2024-09-04 20:19:34 +0700

    fix(TwoSum): correct the expected output in test case

--- solution/TwoSum/TwoSum.test.ts
+++ solution/TwoSum/TwoSum.test.ts
@@ -1,26 +1,25 @@
-import { twoSum } from './TwoSum'; // Assuming twoSum.ts is in the same directory
+import { twoSum } from './TwoSum';
 
-describe('twoSum', () => {
+describe('Two Sum Problems', () => {
   it('should return indices of the two numbers that add up to the target', () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
   });
 
   it('should return indices when the numbers are in the middle of the array', () => {
-    expect(twoSum([1, 2, 3, 6, 8], 9)).toEqual([3, 4]);
+    expect(twoSum([1, 2, 3, 6, 8], 9)).toEqual([2, 3]);
   });
 
   it('should return indices for negative numbers', () => {
-    expect(twoSum([-1, -2, -3, -6, -8], -9)).toEqual([3, 4]);
+    expect(twoSum([-1, -2, -3, -6, -8], -9)).toEqual([2, 3]);
   });
 
   it('should throw an error if no solution is found', () => {
     expect(() => twoSum([1, 2, 3], 10)).toThrow('No two sum solution');
   });
 
-  // **New test case for large numbers**
   it('should return indices for large numbers', () => {
-    const bigNum1 = 9007199254740991;  // Maximum safe integer in JavaScript (2^53 - 1)
-    const bigNum2 = 9007199254740990;
+    const bigNum1 = 4503599627370497; 
+    const bigNum2 = 4503599627370493;
     expect(twoSum([bigNum1, bigNum2, 3], bigNum1 + bigNum2)).toEqual([0, 1]);
   });
 });
